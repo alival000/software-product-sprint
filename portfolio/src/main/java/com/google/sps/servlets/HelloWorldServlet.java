5778bf160dd6a30ee441e5a6e90260a25fb26c39
Alexandra Valdes <alexandra.s.valdes@gmail.com>
2021-06-15 04:11:14 +0000

    Week 2 Portfolio

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.*;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
@@ -12,7 +14,16 @@ public class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    ArrayList<String> messages = new ArrayList<String>();
+    messages.add("You are almost there!");
+    messages.add("You can do it!");
+    messages.add("Everyday is a new day.");
+    messages.add("Good job!");
+    
+    String jsonMessages = new Gson().toJson(messages);
+
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Ali!</h1>");
+    response.getWriter().println(jsonMessages);
   }
 }
